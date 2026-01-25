b7fc5b19857eebb25b7e1fef8f6a9d9c044c6516
Sophia Thi <sophie.x.thi@outlook.com>
2026-01-24 22:57:12 -0600

    Add Assignment 3 calculator UI

---
+++ Assignment3/Assignment3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment3
+//
+//  Created by Sophia Thi on 1/23/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
