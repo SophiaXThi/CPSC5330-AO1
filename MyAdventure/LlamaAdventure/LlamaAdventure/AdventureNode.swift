96663610da6793518b46e2151fc6764ea581cc6b
SophiaXThi <59907682+SophiaXThi@users.noreply.github.com>
2026-02-08 22:19:49 -0600

    Uploaded MyAdventure and all of the files with it
    Llama and cupcakes

---
+++ MyAdventure/LlamaAdventure/LlamaAdventure/AdventureNode.swift
@@ -0,0 +1,24 @@
+//
+//  AdventureNode.swift
+//  LlamaAdventure
+//
+//  Created by Sophia Thi on 2/8/26.
+//
+
+import Foundation
+
+struct AdventureNode {
+    
+    let text: String
+    
+    let choice1: String
+    let choice1Destination: Int
+    
+    let choice2: String
+    let choice2Destination: Int
+    
+    let isEnding: Bool
+    
+    // New image for each scene
+    let imageName: String
+}
